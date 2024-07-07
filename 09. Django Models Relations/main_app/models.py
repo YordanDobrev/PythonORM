74f63c75dfa28c0127755ab21827faeb13fd4253
YordanDobrev <petolisko@gmail.com>
2024-07-07 15:11:00 +0300

    Task Completed

---
+++ 09. Django Models Relations/main_app/models.py
@@ -0,0 +1,53 @@
+from datetime import date
+
+from django.db import models
+
+
+# Create your models here.
+
+class Lecturer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True)
+    lecturer = models.ForeignKey("Lecturer", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.lecturer.first_name} {self.lecturer.last_name}'
+
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=10, primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    email = models.EmailField(unique=True)
+    subjects = models.ManyToManyField("Subject", through="StudentEnrollment")
+
+
+class StudentChoices(models.TextChoices):
+    A = "A", "A"
+    B = "B", "B"
+    C = "C", "C"
+    D = "D", "D"
+    F = "F", "F"
+
+
+class StudentEnrollment(models.Model):
+    student = models.ForeignKey("Student", on_delete=models.CASCADE)
+    subject = models.ForeignKey("Subject", on_delete=models.CASCADE)
+    enrollment_date = models.DateField(default=date.today)
+    grade = models.CharField(max_length=1, choices=StudentChoices.choices, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.student} is enrolled in {self.subject}.'
+
+
+class LecturerProfile(models.Model):
+    lecturer = models.OneToOneField("Lecturer", on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+    office_location = models.CharField(max_length=100, blank=True, null=True)
