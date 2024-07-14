a4b565745212083bac9b4335f7338cbd8b8c6acf
YordanDobrev <petolisko@gmail.com>
2024-07-14 11:46:26 +0300

    Task Complete

---
+++ 11. Models Inheritance and Customization/main_app/models.py
@@ -0,0 +1,90 @@
+from datetime import date
+
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+# Create your models here.
+
+class BooleanChoiceField(models.BooleanField):
+    def __init__(self, *args, **kwargs):
+        kwargs['choices'] = ((True, 'Available'),
+                             (False, 'Not Available'))
+        kwargs['default'] = True
+        super().__init__(*args, **kwargs)
+
+
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    sound = models.CharField(max_length=100)
+
+    @property
+    def age(self):
+        today = date.today()
+        age = today.year - self.birth_date.year - (
+                (today.month, today.day) <
+                (self.birth_date.month, self.birth_date.day))
+        return age
+
+
+class Mammal(Animal):
+    fur_color = models.CharField(max_length=50)
+
+
+class Bird(Animal):
+    wing_span = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Reptile(Animal):
+    scale_type = models.CharField(max_length=50)
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
+
+
+class SpecialtyChoices(models.TextChoices):
+    MAMMALS = "Mammals", "Mammals"
+    BIRDS = "Birds", "Birds"
+    REPTILES = "Reptiles", "Reptiles"
+    OTHERS = "Others", "Others"
+
+
+class ZooKeeper(Employee):
+    specialty = models.CharField(max_length=10, choices=SpecialtyChoices.choices)
+    managed_animals = models.ManyToManyField(Animal)
+
+    def clean(self):
+        choices = [choice[0] for choice in SpecialtyChoices]
+        if self.specialty not in choices:
+            raise ValidationError(
+                "Specialty must be a valid choice."
+            )
+
+
+class Veterinarian(Employee):
+    license_number = models.CharField(max_length=10)
+    availability = BooleanChoiceField()
+
+
+class ZooDisplayAnimal(Animal):
+    class Meta:
+        proxy = True
+
+    def display_info(self):
+        return f"Meet {self.name}! Species: {self.species}, born {self.birth_date}. It makes a noise like '{self.sound}'."
+
+    SPECIES_AT_RISK = ["Cross River Gorilla", "Orangutan",
+                       "Green Turtle"]
+
+    def is_endangered(self):
+        danger = self.species in self.SPECIES_AT_RISK
+        return f"{self.species} is at risk!" if danger \
+            else f"{self.species} is not at risk."
